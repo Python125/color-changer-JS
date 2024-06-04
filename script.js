7ea409bc23b74e73031d478f73436f111ed8e182
PythonMonty <andrewnovosel90@gmail.com>
2024-06-03 22:49:26 -0400

    First commit (changed color of btns themselves)

---
+++ script.js
@@ -0,0 +1,54 @@
+//const colors = ["#FF0000", "#00FF00", "#0000FF", "#FFFF00"];
+
+//const colorbox = document.getElementById("colorbox");
+/*const red = document.getElementById("red");
+const green = document.getElementById("green");
+const blue = document.getElementById("blue");
+const yellow = document.getElementById("yellow");*/
+
+/*red.addEventListener("click", changeToRed);
+green.addEventListener("click", changeToGreen);
+blue.addEventListener("click", changeToBlue);
+yellow.addEventListener("click", changeToYellow);*/
+
+red.addEventListener("click", function () {
+  document.getElementById("red").style.backgroundColor = "#FF0000";
+});
+
+green.addEventListener("click", function () {
+  document.getElementById("green").style.backgroundColor = "#00FF00";
+});
+
+blue.addEventListener("click", function () {
+  document.getElementById("blue").style.backgroundColor = "#0000FF";
+});
+
+yellow.addEventListener("click", function () {
+  document.getElementById("yellow").style.backgroundColor = "#FFFF00";
+});
+
+/*
+function changeToRed() {
+  changeToRed.style.backgroundColor = "#FF0000";
+}
+
+function changeToGreen() {
+  changeToGreen.style.backgroundColor = "#00FF00";
+}
+
+function changeToBlue() {
+  changeToBlue.style.backgroundColor = "#0000FF";
+}
+
+function changeToYellow() {
+  changeToYellow.style.backgroundColor = "#FFFF00";
+}*/
+
+/*function changeColor() {
+    document.getElementById("colorbox");
+
+}
+
+colors.addEventListener("click", function () {
+  colors.style.backgroundColor = "red";
+});*/
